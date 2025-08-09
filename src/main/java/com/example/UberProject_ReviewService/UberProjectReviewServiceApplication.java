5f8e5cf03a58a1ea0b4524b3658e65425bac074c
coderVamsi <vamsiganta909@gmail.com>
2025-08-09 17:07:11 +0530

    Uber basic Review Service

---
+++ src/main/java/com/example/UberProject_ReviewService/UberProjectReviewServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.UberProject_ReviewService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberProjectReviewServiceApplication.class, args);
+
+	}
+
+}
